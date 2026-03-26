5bc3b9c97475521ae44f6e9287d54c8768390306
DESKTOP-SLNADE2\user <qq11123@naver.com>
2026-03-26 21:01:56 +0900

    [chore] 젠킨스 자동 업데이트 테스트

--- MatchMaking/Controllers/PingController.cs
+++ MatchMaking/Controllers/PingController.cs
@@ -20,7 +20,7 @@ namespace MatchMaking.Controllers
             // 응답 메시지와 서버 현재 시각(UTC)을 JSON으로 반환
             return Ok(new
             {
-                Message = "Pong!",
+                Message = "Pong! is New",
                 Timestamp = DateTime.UtcNow
             });
         }
